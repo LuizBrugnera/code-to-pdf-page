5fea69d4078ccc9d9a071d1ad796809782d30b4f
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-01-09 11:55:03 -0300

    code-to-pdf v.1

---
+++ src/components/FileProcessor.tsx
@@ -0,0 +1,185 @@
+import React, { useState } from "react";
+import { PDFDocument, PDFFont, StandardFonts } from "pdf-lib";
+import JSZip from "jszip";
+
+type props = {
+  setGeneratedPDF: React.Dispatch<React.SetStateAction<Uint8Array | null>>;
+  setShowAlert: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const FileProcessorComponent: React.FC<props> = ({
+  setGeneratedPDF,
+  setShowAlert,
+}: props) => {
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+  const excludedFolders = [
+    "node_modules",
+    ".git",
+    ".vscode",
+    "dist",
+    "build",
+    "coverage",
+  ];
+  const excludedFiles = [
+    ".gitignore",
+    "package-lock.json",
+    "tsconfig.json",
+    "README.md",
+    "output.pdf",
+    "*.png",
+    "*.jpg",
+    "*.jpeg",
+    "*.gif",
+    "*.bmp",
+    "*.ico",
+  ];
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setSelectedFiles(Array.from(event.target.files));
+    }
+  };
+
+  const processZipFile = async (
+    file: File,
+    doc: PDFDocument,
+    font: PDFFont,
+    summaryData: { fileCount: number; archiveTypes: Set<string> }
+  ) => {
+    const zip = new JSZip();
+    const zipContents = await zip.loadAsync(file);
+    const fileNames = Object.keys(zipContents.files);
+
+    for (const fileName of fileNames) {
+      if (zipContents.files[fileName].dir) continue;
+
+      if (
+        excludedFolders.some((folder) => fileName.includes(folder)) ||
+        excludedFiles.some((exFile) => fileName.endsWith(exFile))
+      ) {
+        continue;
+      }
+
+      const fileData = await zipContents.files[fileName].async("string");
+      const lines = fileData.split("\n");
+      let currentPage = doc.addPage();
+      let currentY = currentPage.getHeight() - 50;
+      const text = `Arquivo: ${fileName}\n\n`;
+      currentPage.drawText(text, {
+        x: 50,
+        y: currentY,
+        size: 10,
+        font: font,
+        lineHeight: 14,
+        maxWidth: currentPage.getWidth() - 100,
+      });
+      currentY -= 14 * 3;
+
+      for (const line of lines) {
+        if (currentY < 50) {
+          currentPage = doc.addPage();
+          currentY = currentPage.getHeight() - 50;
+        }
+        currentPage.drawText(line, {
+          x: 50,
+          y: currentY,
+          size: 10,
+          font: font,
+          lineHeight: 14,
+          maxWidth: currentPage.getWidth() - 100,
+        });
+        currentY -= 14;
+      }
+      summaryData.fileCount++;
+      summaryData.archiveTypes.add(fileName.split(".").pop() || "");
+    }
+  };
+
+  const processFiles = async () => {
+    if (selectedFiles.length === 0)
+      return setShowAlert(
+        "Por favor, selecione ao menos um arquivo antes de processar."
+      );
+
+    const summaryData = {
+      fileCount: 0,
+      archiveTypes: new Set<string>(),
+    };
+
+    const doc = await PDFDocument.create();
+
+    const font = await doc.embedFont(StandardFonts.Courier);
+
+    for (const file of selectedFiles) {
+      if (file.name.endsWith(".zip")) {
+        await processZipFile(file, doc, font, summaryData);
+      } else {
+        let fileContent = await file.text();
+        fileContent = fileContent.replace(/[^\x20-\x7E\n\t]/g, "");
+
+        const lines = fileContent.split("\n");
+        let currentPage = doc.addPage();
+        const text = `Arquivo: ${file.name}\n\n`;
+        currentPage.drawText(text, {
+          x: 50,
+          y: currentPage.getHeight() - 50,
+          size: 10,
+          font: font,
+          lineHeight: 14,
+          maxWidth: currentPage.getWidth() - 100,
+        });
+
+        let currentY = currentPage.getHeight() - 50 - 14 * 3;
+
+        for (const line of lines) {
+          if (currentY < 50) {
+            currentPage = doc.addPage();
+            currentY = currentPage.getHeight() - 50;
+          }
+          currentPage.drawText(line, {
+            x: 50,
+            y: currentY,
+            size: 10,
+            font: font,
+            lineHeight: 14,
+            maxWidth: currentPage.getWidth() - 100,
+          });
+          currentY -= 14;
+        }
+
+        summaryData.fileCount++;
+        summaryData.archiveTypes.add(file.name.split(".").pop() || "");
+      }
+    }
+
+    const summaryPage = doc.addPage();
+    const summaryText = `Content Summary\n\nTotal Files: ${
+      summaryData.fileCount
+    }\n\nArchive Types Found: ${Array.from(summaryData.archiveTypes).join(
+      ", "
+    )}\n\nSelected Files: ${selectedFiles.map((file) => file.name).join(", ")}`;
+    summaryPage.drawText(summaryText, {
+      x: 50,
+      y: summaryPage.getHeight() - 50,
+      size: 12,
+      font: font,
+      lineHeight: 14,
+      maxWidth: summaryPage.getWidth() - 100,
+    });
+
+    const pdfBytes = await doc.save();
+    setGeneratedPDF(pdfBytes);
+  };
+
+  return (
+    <div>
+      <p>
+        <input type="file" multiple onChange={handleFileChange} />
+      </p>
+      <button onClick={processFiles}>Converter Código</button>
+    </div>
+  );
+};
+
+export default FileProcessorComponent;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./Home.css";
+import FileProcessorComponent from "../FileProcessor";
+
+const Home: React.FC = () => {
+  const [generatedPDF, setGeneratedPDF] = useState<Uint8Array | null>(null);
+  const [showAlert, setShowAlert] = useState<string>("");
+
+  const downloadPDF = () => {
+    if (!generatedPDF)
+      setShowAlert("Selecione um arquivo e o converta antes de baixar");
+    if (generatedPDF) {
+      const blob = new Blob([generatedPDF], { type: "application/pdf" });
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = "output.pdf";
+      link.click();
+    }
+  };
+
+  return (
+    <div className="home-container">
+      <header>
+        <nav>
+          <div className="logo">CodeToPDF</div>
+          <div className="menu"></div>
+        </nav>
+      </header>
+      <main>
+        <section className="converter">
+          <h1>Conversor de Codigo para PDF</h1>
+          <p>Converta seus projetos e codigos para PDF online e grátis</p>
+          <p>
+            Uma das partes mais uteis da conversão para PDF é para usar o
+            CHATGPT ou similares com o contexto do projeto inteiro para resolver
+            as suas duvidas
+          </p>
+
+          <div className="file-types">
+            <div className="file-type">
+              <h2>Project.zip</h2>
+              <p>Insira seu projeto .zip aqui e clique em processar</p>
+              <FileProcessorComponent setGeneratedPDF={setGeneratedPDF} setShowAlert={setShowAlert}/>
+            </div>
+            <div className="file-type">
+              <h2>Project.pdf</h2>
+              <p>Projeto convertido para PDF</p>
+
+              <button onClick={downloadPDF}>Baixar Pdf</button>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="footer-content">
+          <p>
+            Visit my personal website:{" "}
+            <a href="http://luizbrugnera.com" target="_blank">
+              luizbrugnera.com
+            </a>
+          </p>
+          <p>Created by Luiz Ricardo Brugnera</p>
+          <p>Contact me: 54 9927-6395</p>
+        </div>
+      </footer>
+      {showAlert && (
+        <div className="alert-modal">
+          <div className="alert-content">
+            <p>{showAlert}</p>
+            <button onClick={() => setShowAlert("")}>Fechar</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
